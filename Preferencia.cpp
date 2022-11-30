1b3806703ef610fea4f20aca3115ac362ebf7746
doki-JP <111583401+doki-JP@users.noreply.github.com>
2022-11-29 21:19:14 -0600

    Update Preferencia.cpp
    Implementacion de métodos, tanto getters como setters

--- Preferencia.cpp
+++ Preferencia.cpp
@@ -4,23 +4,22 @@
 using namespace std;
 
 Preferencia::Preferencia(){
-    Genero=-1;
-
+    Genero=0;
+    Lenguaje=0;
 };
-
-Preferencia::Preferencia(int G){
-    Genero=G;
-
+Preferencia::Preferencia(int GP,int LP){
+    Genero=GP;
+    Lenguaje=LP;   
 }
-
-void Preferencia::setGenero(int G){
-    cout<<"0. Salir\n1. Pop en Ingles\n2. Rap en Ingles\n3. EDM en Ingles\n4. Alternativo en Ingles";
-    cout<<"\n5. Pop en Espanol\n6. Rap en Espanol\n7. EDM en Espanol\n8. Alternativo en Espanol\n";
-    cout<<"\nSeleccion: ";
-    cin>>G;
-    Genero=G;
+int Preferencia::getLenguaje(){
+    return Lenguaje;
 }
 int Preferencia::getGenero(){
     return Genero;
-    
+}
+void Preferencia::setGenero(int GP){
+    Genero=GP;
+}
+void Preferencia::setLenguaje(int LP){
+    Lenguaje=LP;
 }
