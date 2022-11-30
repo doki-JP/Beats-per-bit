f391842623ef6614d38340d8819a91ae4f4bf6f4
doki-JP <111583401+doki-JP@users.noreply.github.com>
2022-11-30 00:56:24 -0600

    Update Genero.cpp
    Implementacion de Herencia

---
+++ Genero.cpp
@@ -0,0 +1,14 @@
+#include "Genero.h"
+Genero::Genero(){
+    GeneroPreferido= 0;
+}
+Genero::Genero(int GP, int LP):Preferencia(GP,LP){
+    GeneroPreferido=GP;
+}
+int Genero::getGenero(){
+    return GeneroPreferido;
+}
+
+void Genero::setGenero(int GP){
+    GeneroPreferido=GP;
+}
