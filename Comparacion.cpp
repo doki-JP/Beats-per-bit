577029e0f9670f515bf6d412f087331fad33215d
doki-JP <111583401+doki-JP@users.noreply.github.com>
2022-11-30 00:57:44 -0600

    Update Comparacion.cpp
    Uso de Composición para generar cambio dentro de otra clase

--- Comparacion.cpp
+++ Comparacion.cpp
@@ -1,20 +1,19 @@
 #include "Comparacion.h"
 #include <iostream>
 
-Comparacion::Comparacion(){
-    Gusto.setGenero(0);
-    Gusto.setLenguaje(0);
-}
-
-int Comparacion::getLenguaje(){
-    return Gusto.getLenguaje();
-}
-int Comparacion::getGenero(){
-    return Gusto.getGenero();
-}
+int Preferencia::Idiom(int L,int Len){
+    if (Len==0){}
+    else if(Len==1){
+        if (L==0){L=1;}
+        else if(L==1){L=0;}
+    }
+    return L;
+};
 
 string Comparacion::Cancion(int G,int L){
     int S=1;
+    int CambioL=0;
+    int Len=0;
     string CancionesE[4][3]={{"Brillaremos - Julieta Venegas\n",
         "Sembrando FLores - Natalia Lafourcade\n","Hasta la Raiz - Natalia Lafourcade\n"},
         {"Rap Bruto - Residente\n","Whisky and Spice - Yeico & Toni\n",
@@ -27,19 +26,23 @@ string Comparacion::Cancion(int G,int L){
         "GAGGED & BOUND - MUST DIE!\n"},{"Moonlight - Baby Panna\n","Not Allowed - TV Girl\n",
         "Self - Khalid\n"}};
     while (S!=0){
-    G=G-1;
-    if (L==0){
-        cout<<CancionesI[G][0];
-        cout<<CancionesI[G][1];
-        cout<<CancionesI[G][2];}
+        G=G-1;
+        if (L==0){
+            cout<<CancionesI[G][0];
+            cout<<CancionesI[G][1];
+            cout<<CancionesI[G][2];}
 
-    else if (L==1){
-        cout<<CancionesE[G][0];
-        cout<<CancionesE[G][1];
-        cout<<CancionesE[G][2];}
-    cout<<"Presione '0' para cerrar el programa\n";
-    cout<<"O seleccione otro genero del mismo idioma\n1. Pop - 2. Rap\n3. EDM - 4. Alternativo\nSeleccion: ";
-    cin>>G;
-    S=G;
-    }
+        else if (L==1){
+            cout<<CancionesE[G][0];
+            cout<<CancionesE[G][1];
+            cout<<CancionesE[G][2];}
+        cout<<"Presione '0' para cerrar el programa.\n";
+        cout<<"Seleccione otro genero\n1. Pop - 2. Rap\n3. EDM - 4. Alternativo\nSeleccion: ";
+        cin>>G;
+        cout<<"\nDeseas cambiar de idioma? Seleccion: ";
+        cout<<"\n0. No - 1. Si\nSeleccion: ";
+        cin>>Len;
+        S=G; 
+        L=obj.Idiom(L,Len);
+        }
 }
