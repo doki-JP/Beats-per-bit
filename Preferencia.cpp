06aff4b0d62a69f2cbed707dfb00762b41f095f9
doki-JP <111583401+doki-JP@users.noreply.github.com>
2022-11-30 01:06:12 -0600

    Update Preferencia.cpp
    Desecho de metodos basura, implementacion del archivo header y creacion de objeto para su uso en composicion

--- Preferencia.cpp
+++ Preferencia.cpp
@@ -7,14 +7,8 @@ Preferencia::Preferencia(){
     Genero=0;
     Lenguaje=0;
 };
-void Preferencia::setLenguaje(int L){
-    Lenguaje=L;
-}
 Preferencia::Preferencia(int GP,int LP){
     Genero=GP;
     Lenguaje=LP;  
 }
-int Preferencia::getLenguaje(){
-    return Lenguaje;
-};
 Preferencia obj; 
