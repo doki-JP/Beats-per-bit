fdad73e9fb42c0701a26d1d6f486c49bf2ac568c
doki-JP <111583401+doki-JP@users.noreply.github.com>
2022-11-30 01:01:21 -0600

    Update Lenguaje.h
    Creacion de archivo header, Ademas de incluir herencia, se acomodaron setters y getters

---
+++ Lenguaje.h
@@ -0,0 +1,15 @@
+#ifndef LENGUAJE_
+#define LENGUAJE_
+#include <iostream>
+#include "Preferencia.h"
+class Lenguaje:public Preferencia{
+    private:
+        int LenguajePreferido;
+    public:
+        Lenguaje();
+        Lenguaje(int GP, int L);
+        int getLenguaje();
+        void setLenguaje(int);
+};
+
+#endif
